3e410a2cc3d67f01f4b3431b4fb4c6fb11b4a5a3
zkan <kan@prontomarketing.com>
2019-05-14 08:21:21 +0700

    :pencil: Specify elements as list of any :bear:

--- python/all-the-same/all_the_same.py
+++ python/all-the-same/all_the_same.py
@@ -1,5 +1,5 @@
-from typing import List
+from typing import Any, List
 
 
-def all_the_same(elements: List) -> bool:
+def all_the_same(elements: List[Any]) -> bool:
     return all(map(lambda x: elements[0] == x, elements))
