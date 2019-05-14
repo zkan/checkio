a8542f4228ebfba5539f1d2a0e1b2f5fd7994eee
zkan <kan@prontomarketing.com>
2019-05-14 08:08:04 +0700

    :sparkles: :recycle: Return true when get empty list :bear:

--- python/all-the-same/all_the_same.py
+++ python/all-the-same/all_the_same.py
@@ -2,9 +2,8 @@ from typing import List
 
 
 def all_the_same(elements: List) -> bool:
-    first_element = elements[0]
     for each in elements:
-        if first_element != each:
+        if elements[0] != each:
             return False
     else:
         return True
--- python/all-the-same/test_all_the_same.py
+++ python/all-the-same/test_all_the_same.py
@@ -9,3 +9,8 @@ def test_it_should_return_true_when_all_elements_are_the_same():
 def test_it_should_return_false_when_all_elements_are_not_the_same():
     result = all_the_same([1, 2, 1])
     assert result == False
+
+
+def test_it_should_return_true_when_no_element():
+    result = all_the_same([])
+    assert result == True
